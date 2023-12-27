7ac8568911374c7de73d85d3b67ac82b43ed26b9
VishnuSage <vishnusatheeshkumar007@gmail.com>
2023-12-27 20:01:41 +0530

    mern-chat-app

---
+++ backend/models/userModel.js
@@ -0,0 +1,30 @@
+const mongoose =  require('mongoose');
+const bcrypt = require('bcryptjs');
+const crypto = require('crypto');
+
+const userSchema = mongoose.Schema(
+    {
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    pic: { type: String, default: "https://icon-library.com/images/anonymous-avatar-icon/anonymous-avatar-icon-25.jpg" },
+    },
+    { timestamps: true }
+);
+
+userSchema.pre('save', async function (next) {
+    if (this.isModified('password')) {
+       const salt = await bcrypt.genSalt(10);
+       this.password = await bcrypt.hash(this.password, salt);
+    }
+    next();
+   });
+   
+   userSchema.methods.matchPassword = function (candidatePassword) {
+    return bcrypt.compare(candidatePassword, this.password);
+
+};
+
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
